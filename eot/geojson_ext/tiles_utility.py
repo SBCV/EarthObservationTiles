d30e1a28b6b653d4dcc374af76f489a8ad2c1e64
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2022-11-18 11:13:20 +0100

    Move and adapt tiles utility

---
+++ eot/geojson_ext/tiles_utility.py
@@ -0,0 +1,99 @@
+import os
+from eot.tiles.tile_manager import TileManager
+from eot.tools.aggregate_utility.geojson import create_grid_geojson
+from eot.utility.os_extension import makedirs_safely
+from eot.tiles.tile_path_manager import TilePathManager
+from eot.tiles.image_pixel_tile import ImagePixelTile
+from eot.tiles.mercator_tile import MercatorTile
+from eot.utility.os_extension import get_regex_fps_in_dp
+from eot.tools.tools_api import run_aggregate
+
+
+def _create_geojson_tile_grid(idp, geojson_ofp, original_ifp=None):
+    image_raster_name = None
+    if original_ifp:
+        image_raster_name = os.path.splitext(os.path.basename(original_ifp))[0]
+    image_tiles = list(
+        TileManager.read_tiles_from_dir(
+            idp=idp,
+            target_raster_name=image_raster_name,
+        )
+    )
+    makedirs_safely(os.path.dirname(geojson_ofp))
+    create_grid_geojson(image_tiles, geojson_ofp, original_ifp)
+
+
+def create_geojson_for_image_tiles(
+    data_dp, search_regex, ignore_regex, images_dp, grid_json_fn
+):
+    dataset_tile_type = TilePathManager.get_tile_type_from_dir(
+        images_dp, is_dataset_dp=True
+    )
+    if dataset_tile_type == ImagePixelTile:
+        original_ifps = get_regex_fps_in_dp(
+            data_dp, search_regex, ignore_regex
+        )
+        for original_ifp in original_ifps:
+            image_raster_name = os.path.splitext(
+                os.path.basename(original_ifp)
+            )[0]
+            geojson_odp = os.path.join(
+                images_dp, "geojson_summary", image_raster_name
+            )
+            geojson_ofp = os.path.join(geojson_odp, grid_json_fn)
+            _create_geojson_tile_grid(images_dp, geojson_ofp, original_ifp)
+    elif dataset_tile_type == MercatorTile:
+        geojson_odp = os.path.join(images_dp, "geojson_summary")
+        geojson_ofp = os.path.join(geojson_odp, grid_json_fn)
+        _create_geojson_tile_grid(images_dp, geojson_ofp)
+    else:
+        assert False
+
+
+def create_geojson_for_label_tiles(
+    test_data_dp,
+    search_regex,
+    ignore_regex,
+    test_images_dp,
+    test_labels_dp,
+    toml_config_fp,
+    training_category_titles,
+    grid_json_fn,
+    lazy=False,
+):
+    dataset_tile_type = TilePathManager.get_tile_type_from_dir(
+        test_images_dp, is_dataset_dp=True
+    )
+    if dataset_tile_type == ImagePixelTile:
+        original_ifps = get_regex_fps_in_dp(
+            test_data_dp, search_regex, ignore_regex
+        )
+        for original_ifp in original_ifps:
+            masks_raster_name = os.path.splitext(
+                os.path.basename(original_ifp)
+            )[0]
+            geojson_odp = os.path.join(
+                test_labels_dp, "geojson_summary", masks_raster_name
+            )
+            run_aggregate(
+                config_fp=toml_config_fp,
+                masks_idp=test_labels_dp,
+                requested_category_titles=training_category_titles,
+                masks_raster_name=masks_raster_name,
+                geojson_odp=geojson_odp,
+                geojson_grid_ofn=grid_json_fn,
+                original_raster_ifp=original_ifp,
+                lazy=lazy,
+            )
+    elif dataset_tile_type == MercatorTile:
+        geojson_odp = os.path.join(test_labels_dp, "geojson_summary")
+        run_aggregate(
+            config_fp=toml_config_fp,
+            masks_idp=test_labels_dp,
+            requested_category_titles=training_category_titles,
+            geojson_odp=geojson_odp,
+            geojson_grid_ofn=grid_json_fn,
+            lazy=lazy,
+        )
+    else:
+        assert False
