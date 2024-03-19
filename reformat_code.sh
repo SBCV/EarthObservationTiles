4a29e53bb7800f31e2947672ee5f6cc5cb92dcf5
Sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2024-03-19 18:54:57 +0100

    Update reformat_code.sh

--- reformat_code.sh
+++ reformat_code.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Go to the directory where the script is located
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
-black --line-length 79 eot
-black --line-length 79 examples
-black --line-length 79 data_preparation
+REQUIRED_VERSION="24.3.0"
+black --required-version ${REQUIRED_VERSION} --line-length 79 eot
+black --required-version ${REQUIRED_VERSION} --line-length 79 examples
+black --required-version ${REQUIRED_VERSION} --line-length 79 data_preparation
